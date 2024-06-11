fbf6427ca31d3ecbb334b4fd785c6958c09146fe
6262557 <6262557@LLBINFD060419>
2024-06-11 10:10:30 -0400

    Implémentation de la Vue Speciality et de son Contrôleur

---
+++ JulieProSol/JuliePro/Data/MyDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using JuliePro.Models;
+
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext (DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+        }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Speciality>().HasData(new Speciality() { Id = 1, Name = "Perte de poids" });
+        builder.Entity<Speciality>().HasData(new Speciality() { Id = 2, Name = "Course" });
+        builder.Entity<Speciality>().HasData(new Speciality() { Id = 3, Name = "Halthérophilie" });
+        builder.Entity<Speciality>().HasData(new Speciality() { Id = 4, Name = "Réhabilitation" });
+    }
+    public DbSet<JuliePro.Models.Speciality> Speciality { get; set; } = default!;
+    }
---
+++ JulieProSol/JuliePro/Models/Speciality.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JuliePro.Models
+{
+    public class Speciality
+    {
+        public int Id { get; set; }
+
+        [StringLength(20, MinimumLength = 5, ErrorMessage ="Le nom de la spécialité doit être compris entre {2} et {1} caractères")]
+        public string Name { get; set; }
+    }
+}
